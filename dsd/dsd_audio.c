d26bb6aa26188111bb7ad909b8f91894e475f046
robotastic <luke@robotastic.com>
2014-03-15 20:50:37 -0400

    more

--- dsd/dsd_audio.c
+++ dsd/dsd_audio.c
@@ -17,139 +17,139 @@
 
 #include "dsd.h"
 
-void
-processAudio (dsd_opts * opts, dsd_state * state)
-{
+ void
+ processAudio (dsd_opts * opts, dsd_state * state)
+ {
 
   int i, n;
   float aout_abs, max, gainfactor, gaindelta, maxbuf;
 
   if (opts->audio_gain == (float) 0)
-    {
+  {
       // detect max level
-      max = 0;
-      state->audio_out_temp_buf_p = state->audio_out_temp_buf;
-      for (n = 0; n < 160; n++)
-        {
-          aout_abs = fabsf (*state->audio_out_temp_buf_p);
-          if (aout_abs > max)
-            {
-              max = aout_abs;
-            }
-          state->audio_out_temp_buf_p++;
-        }
-      *state->aout_max_buf_p = max;
-      state->aout_max_buf_p++;
-      state->aout_max_buf_idx++;
-      if (state->aout_max_buf_idx > 24)
-        {
-          state->aout_max_buf_idx = 0;
-          state->aout_max_buf_p = state->aout_max_buf;
-        }
+    max = 0;
+    state->audio_out_temp_buf_p = state->audio_out_temp_buf;
+    for (n = 0; n < 160; n++)
+    {
+      aout_abs = fabsf (*state->audio_out_temp_buf_p);
+      if (aout_abs > max)
+      {
+        max = aout_abs;
+      }
+      state->audio_out_temp_buf_p++;
+    }
+    *state->aout_max_buf_p = max;
+    state->aout_max_buf_p++;
+    state->aout_max_buf_idx++;
+    if (state->aout_max_buf_idx > 24)
+    {
+      state->aout_max_buf_idx = 0;
+      state->aout_max_buf_p = state->aout_max_buf;
+    }
 
       // lookup max history
-      for (i = 0; i < 25; i++)
-        {
-          maxbuf = state->aout_max_buf[i];
-          if (maxbuf > max)
-            {
-              max = maxbuf;
-            }
-        }
+    for (i = 0; i < 25; i++)
+    {
+      maxbuf = state->aout_max_buf[i];
+      if (maxbuf > max)
+      {
+        max = maxbuf;
+      }
+    }
 
       // determine optimal gain level
-      if (max > (float) 0)
-        {
-          gainfactor = ((float) 30000 / max);
-        }
-      else
-        {
-          gainfactor = (float) 50;
-        }
-      if (gainfactor < state->aout_gain)
-        {
-          state->aout_gain = gainfactor;
-          gaindelta = (float) 0;
-        }
-      else
-        {
-          if (gainfactor > (float) 50)
-            {
-              gainfactor = (float) 50;
-            }
-          gaindelta = gainfactor - state->aout_gain;
-          if (gaindelta > ((float) 0.05 * state->aout_gain))
-            {
-              gaindelta = ((float) 0.05 * state->aout_gain);
-            }
-        }
-      gaindelta /= (float) 160;
-    }
-  else
+    if (max > (float) 0)
     {
+      gainfactor = ((float) 30000 / max);
+    }
+    else
+    {
+      gainfactor = (float) 50;
+    }
+    if (gainfactor < state->aout_gain)
+    {
+      state->aout_gain = gainfactor;
       gaindelta = (float) 0;
     }
+    else
+    {
+      if (gainfactor > (float) 50)
+      {
+        gainfactor = (float) 50;
+      }
+      gaindelta = gainfactor - state->aout_gain;
+      if (gaindelta > ((float) 0.05 * state->aout_gain))
+      {
+        gaindelta = ((float) 0.05 * state->aout_gain);
+      }
+    }
+    gaindelta /= (float) 160;
+  }
+  else
+  {
+    gaindelta = (float) 0;
+  }
 
   if(opts->audio_gain >= 0){
     // adjust output gain
     state->audio_out_temp_buf_p = state->audio_out_temp_buf;
     for (n = 0; n < 160; n++)
-      {
-        *state->audio_out_temp_buf_p = (state->aout_gain + ((float) n * gaindelta)) * (*state->audio_out_temp_buf_p);
-        state->audio_out_temp_buf_p++;
-      }
+    {
+      *state->audio_out_temp_buf_p = (state->aout_gain + ((float) n * gaindelta)) * (*state->audio_out_temp_buf_p);
+      state->audio_out_temp_buf_p++;
+    }
     state->aout_gain += ((float) 160 * gaindelta);
   }
 
   // copy audio datat to output buffer and upsample if necessary
   state->audio_out_temp_buf_p = state->audio_out_temp_buf;
   if (opts->split == 0)
+  {
+    for (n = 0; n < 160; n++)
     {
-      for (n = 0; n < 160; n++)
-        {
-          upsample (state, *state->audio_out_temp_buf_p);
-          state->audio_out_temp_buf_p++;
-          state->audio_out_float_buf_p += 6;
-          state->audio_out_idx += 6;
-          state->audio_out_idx2 += 6;
-        }
-      state->audio_out_float_buf_p -= (960 + opts->playoffset);
+      upsample (state, *state->audio_out_temp_buf_p);
+      state->audio_out_temp_buf_p++;
+      state->audio_out_float_buf_p += 6;
+      state->audio_out_idx += 6;
+      state->audio_out_idx2 += 6;
+    }
+    state->audio_out_float_buf_p -= (960 + opts->playoffset);
       // copy to output (short) buffer
-      for (n = 0; n < 960; n++)
-        {
-          if (*state->audio_out_float_buf_p > (float) 32760)
-            {
-              *state->audio_out_float_buf_p = (float) 32760;
-            }
-          else if (*state->audio_out_float_buf_p < (float) -32760)
-            {
-              *state->audio_out_float_buf_p = (float) -32760;
-            }
-          *state->audio_out_buf_p = (short) *state->audio_out_float_buf_p;
-          state->audio_out_buf_p++;
-          state->audio_out_float_buf_p++;
-        }
-      state->audio_out_float_buf_p += opts->playoffset;
-    }
-  else
+    for (n = 0; n < 960; n++)
     {
-      for (n = 0; n < 160; n++)
-        {
-          if (*state->audio_out_temp_buf_p > (float) 32760)
-            {
-              *state->audio_out_temp_buf_p = (float) 32760;
-            }
-          else if (*state->audio_out_temp_buf_p < (float) -32760)
-            {
-              *state->audio_out_temp_buf_p = (float) -32760;
-            }
-          *state->audio_out_buf_p = (short) *state->audio_out_temp_buf_p;
-          state->audio_out_buf_p++;
-          state->audio_out_temp_buf_p++;
-          state->audio_out_idx++;
-          state->audio_out_idx2++;
-        }
+      if (*state->audio_out_float_buf_p > (float) 32760)
+      {
+        *state->audio_out_float_buf_p = (float) 32760;
+      }
+      else if (*state->audio_out_float_buf_p < (float) -32760)
+      {
+        *state->audio_out_float_buf_p = (float) -32760;
+      }
+      *state->audio_out_buf_p = (short) *state->audio_out_float_buf_p;
+      state->audio_out_buf_p++;
+      state->audio_out_float_buf_p++;
     }
+    state->audio_out_float_buf_p += opts->playoffset;
+  }
+  else
+  {
+    for (n = 0; n < 160; n++)
+    {
+      if (*state->audio_out_temp_buf_p > (float) 32760)
+      {
+        *state->audio_out_temp_buf_p = (float) 32760;
+      }
+      else if (*state->audio_out_temp_buf_p < (float) -32760)
+      {
+        *state->audio_out_temp_buf_p = (float) -32760;
+      }
+      *state->audio_out_buf_p = (short) *state->audio_out_temp_buf_p;
+      state->audio_out_buf_p++;
+      state->audio_out_temp_buf_p++;
+      state->audio_out_idx++;
+      state->audio_out_idx2++;
+    }
+  }
 }
 
 void
@@ -169,16 +169,16 @@ writeSynthesizedVoice (dsd_opts * opts, dsd_state * state)
   for (n = 0; n < 160; n++)
   {
     if (*state->audio_out_temp_buf_p > (float) 32767)
-      {
-        *state->audio_out_temp_buf_p = (float) 32767;
-      }
+    {
+      *state->audio_out_temp_buf_p = (float) 32767;
+    }
     else if (*state->audio_out_temp_buf_p < (float) -32768)
-      {
-        *state->audio_out_temp_buf_p = (float) -32768;
-      }
-      *aout_buf_p = (short) *state->audio_out_temp_buf_p;
-      aout_buf_p++;
-      state->audio_out_temp_buf_p++;
+    {
+      *state->audio_out_temp_buf_p = (float) -32768;
+    }
+    *aout_buf_p = (short) *state->audio_out_temp_buf_p;
+    aout_buf_p++;
+    state->audio_out_temp_buf_p++;
   }
 
   sf_write_short(opts->wav_out_f, aout_buf, 160);
@@ -219,28 +219,28 @@ playSynthesizedVoice (dsd_opts * opts, dsd_state * state)
   ssize_t result;
 
   if (state->audio_out_idx > opts->delay)
-    {
+  {
       // output synthesized speech to sound card
-      if (opts->audio_out_fd == -1)
-        {
-          memcpy(state->output_buffer + state->output_offset, (state->audio_out_buf_p - state->audio_out_idx), (state->audio_out_idx * 2));
-          state->output_offset += state->audio_out_idx;
-        }
-      else
-        {
-          result = write (opts->audio_out_fd, (state->audio_out_buf_p - state->audio_out_idx), (state->audio_out_idx * 2));
-        }
-      state->audio_out_idx = 0;
+    if (opts->audio_out_fd == -1)
+    {
+      memcpy(state->output_buffer + state->output_offset, (state->audio_out_buf_p - state->audio_out_idx), (state->audio_out_idx * 2));
+      state->output_offset += state->audio_out_idx;
     }
+    else
+    {
+      result = write (opts->audio_out_fd, (state->audio_out_buf_p - state->audio_out_idx), (state->audio_out_idx * 2));
+    }
+    state->audio_out_idx = 0;
+  }
 
   if (state->audio_out_idx2 >= 800000)
-    {
-      state->audio_out_float_buf_p = state->audio_out_float_buf + 100;
-      state->audio_out_buf_p = state->audio_out_buf + 100;
-      memset (state->audio_out_float_buf, 0, 100 * sizeof (float));
-      memset (state->audio_out_buf, 0, 100 * sizeof (short));
-      state->audio_out_idx2 = 0;
-    }
+  {
+    state->audio_out_float_buf_p = state->audio_out_float_buf + 100;
+    state->audio_out_buf_p = state->audio_out_buf + 100;
+    memset (state->audio_out_float_buf, 0, 100 * sizeof (float));
+    memset (state->audio_out_buf, 0, 100 * sizeof (short));
+    state->audio_out_idx2 = 0;
+  }
 }
 
 void
@@ -262,10 +262,10 @@ openAudioOutDevice (dsd_opts * opts, int speed)
 
   opts->audio_out_fd = open (opts->audio_out_dev, O_WRONLY);
   if (opts->audio_out_fd == -1)
-    {
-      printf ("Error, couldn't open %s\n", opts->audio_out_dev);
-      exit (1);
-    }
+  {
+    printf ("Error, couldn't open %s\n", opts->audio_out_dev);
+    exit (1);
+  }
 
   // get current
   ioctl (opts->audio_out_fd, AUDIO_GETINFO, &aset);
@@ -280,10 +280,10 @@ openAudioOutDevice (dsd_opts * opts, int speed)
   aset.play.encoding = AUDIO_ENCODING_LINEAR;
 
   if (ioctl (opts->audio_out_fd, AUDIO_SETINFO, &aset) == -1)
-    {
-      printf ("Error setting sample device parameters\n");
-      exit (1);
-    }
+  {
+    printf ("Error setting sample device parameters\n");
+    exit (1);
+  }
 #endif
 
 #if defined(BSD) && !defined(__APPLE__)
@@ -292,32 +292,32 @@ openAudioOutDevice (dsd_opts * opts, int speed)
 
   opts->audio_out_fd = open (opts->audio_out_dev, O_WRONLY);
   if (opts->audio_out_fd == -1)
-    {
-      printf ("Error, couldn't open %s\n", opts->audio_out_dev);
-      opts->audio_out = 0;
-      exit(1);
-    }
+  {
+    printf ("Error, couldn't open %s\n", opts->audio_out_dev);
+    opts->audio_out = 0;
+    exit(1);
+  }
 
   fmt = 0;
   if (ioctl (opts->audio_out_fd, SNDCTL_DSP_RESET) < 0)
-    {
-      printf ("ioctl reset error \n");
-    }
+  {
+    printf ("ioctl reset error \n");
+  }
   fmt = speed;
   if (ioctl (opts->audio_out_fd, SNDCTL_DSP_SPEED, &fmt) < 0)
-    {
-      printf ("ioctl speed error \n");
-    }
+  {
+    printf ("ioctl speed error \n");
+  }
   fmt = 0;
   if (ioctl (opts->audio_out_fd, SNDCTL_DSP_STEREO, &fmt) < 0)
-    {
-      printf ("ioctl stereo error \n");
-    }
+  {
+    printf ("ioctl stereo error \n");
+  }
   fmt = AFMT_S16_LE;
   if (ioctl (opts->audio_out_fd, SNDCTL_DSP_SETFMT, &fmt) < 0)
-    {
-      printf ("ioctl setfmt error \n");
-    }
+  {
+    printf ("ioctl setfmt error \n");
+  }
 
 #endif
   printf ("Audio Out Device: %s\n", opts->audio_out_dev);
@@ -338,35 +338,35 @@ openAudioInDevice (dsd_opts * opts)
     opts->audio_in_file_info->channels = 1;
     opts->audio_in_file = sf_open(opts->audio_in_dev, SFM_READ, opts->audio_in_file_info);
     if(opts->audio_in_file == NULL) {
-        printf ("Error, couldn't open file %s\n", opts->audio_in_dev);
-        exit(1);
+      printf ("Error, couldn't open file %s\n", opts->audio_in_dev);
+      exit(1);
     }
   }
   else { // this is a device, use old handling
-  opts->audio_in_type = 0;
+    opts->audio_in_type = 0;
 #ifdef SOLARIS
     sample_info_t aset, aget;
     int rgain;
-  
+    
     rgain = 64;
-  
+    
     if (opts->split == 1)
-      {
-        opts->audio_in_fd = open (opts->audio_in_dev, O_RDONLY);
-      }
+    {
+      opts->audio_in_fd = open (opts->audio_in_dev, O_RDONLY);
+    }
     else
-      {
-        opts->audio_in_fd = open (opts->audio_in_dev, O_RDWR);
-      }
+    {
+      opts->audio_in_fd = open (opts->audio_in_dev, O_RDWR);
+    }
     if (opts->audio_in_fd == -1)
-      {
-        printf ("Error, couldn't open %s\n", opts->audio_in_dev);
-        exit(1);
-      }
-  
+    {
+      printf ("Error, couldn't open %s\n", opts->audio_in_dev);
+      exit(1);
+    }
+    
     // get current
     ioctl (opts->audio_in_fd, AUDIO_GETINFO, &aset);
-  
+    
     aset.record.sample_rate = 48000;
     aset.play.sample_rate = 48000;
     aset.record.channels = 1;
@@ -377,60 +377,60 @@ openAudioInDevice (dsd_opts * opts)
     aset.play.encoding = AUDIO_ENCODING_LINEAR;
     aset.record.port = AUDIO_LINE_IN;
     aset.record.gain = rgain;
-  
+    
     if (ioctl (opts->audio_in_fd, AUDIO_SETINFO, &aset) == -1)
-      {
-        printf ("Error setting sample device parameters\n");
-        exit (1);
-      }
+    {
+      printf ("Error setting sample device parameters\n");
+      exit (1);
+    }
 #endif
 
 #if defined(BSD) && !defined(__APPLE__)
     int fmt;
-  
+    
     if (opts->split == 1)
-      {
-        opts->audio_in_fd = open (opts->audio_in_dev, O_RDONLY);
-      }
+    {
+      opts->audio_in_fd = open (opts->audio_in_dev, O_RDONLY);
+    }
     else
-      {
-        opts->audio_in_fd = open (opts->audio_in_dev, O_RDWR);
-      }
-  
+    {
+      opts->audio_in_fd = open (opts->audio_in_dev, O_RDWR);
+    }
+    
     if (opts->audio_in_fd == -1)
-      {
-        printf ("Error, couldn't open %s\n", opts->audio_in_dev);
-        opts->audio_out = 0;
-      }
-  
+    {
+      printf ("Error, couldn't open %s\n", opts->audio_in_dev);
+      opts->audio_out = 0;
+    }
+    
     fmt = 0;
     if (ioctl (opts->audio_in_fd, SNDCTL_DSP_RESET) < 0)
-      {
-        printf ("ioctl reset error \n");
-      }
+    {
+      printf ("ioctl reset error \n");
+    }
     fmt = 48000;
     if (ioctl (opts->audio_in_fd, SNDCTL_DSP_SPEED, &fmt) < 0)
-      {
-        printf ("ioctl speed error \n");
-      }
+    {
+      printf ("ioctl speed error \n");
+    }
     fmt = 0;
     if (ioctl (opts->audio_in_fd, SNDCTL_DSP_STEREO, &fmt) < 0)
-      {
-        printf ("ioctl stereo error \n");
-      }
+    {
+      printf ("ioctl stereo error \n");
+    }
     fmt = AFMT_S16_LE;
     if (ioctl (opts->audio_in_fd, SNDCTL_DSP_SETFMT, &fmt) < 0)
-      {
-        printf ("ioctl setfmt error \n");
-      }
+    {
+      printf ("ioctl setfmt error \n");
+    }
 #endif
   }
   if (opts->split == 1)
-    {
-      printf ("Audio In Device: %s\n", opts->audio_in_dev);
-    }
+  {
+    printf ("Audio In Device: %s\n", opts->audio_in_dev);
+  }
   else
-    {
-      printf ("Audio In/Out Device: %s\n", opts->audio_in_dev);
-    }
+  {
+    printf ("Audio In/Out Device: %s\n", opts->audio_in_dev);
+  }
 }
